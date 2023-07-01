ac95677c2dea1312d55606295877b3a5f82c3c07
newsun87 <newsun87@mail.sju.edu.tw>
2023-07-01 09:58:07 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -43,7 +43,7 @@ def handle_message(event):
         working_status = True
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="我收到的訊息:" + event.message.text))  
+            TextSendMessage(text="我收到的訊息： " + event.message.text))  
         return       
         
 if __name__ == "__main__": 
