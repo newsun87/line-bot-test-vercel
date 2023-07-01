8b510bd868387da8f283f7ce7c01da9bf131db83
newsun87 <newsun87@mail.sju.edu.tw>
2023-07-01 09:42:27 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -47,11 +47,12 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))  
         return 
-    '''else: 
+    else: 
         working_status = True
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=f"我收到的訊息：{event.message.text}")'''
+            TextSendMessage(text="我收到的訊息：+ event.message.text")
+            return
           
         
 if __name__ == "__main__": 
